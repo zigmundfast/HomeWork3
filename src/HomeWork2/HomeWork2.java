6ee1d534d12a7f3e501636ca6d1e753882667f04
zigmundfast <tax_74@mail.ru>
2022-07-22 17:06:42 +0500

    HomeWork2+

--- src/HomeWork2/HomeWork2.java
+++ src/HomeWork2/HomeWork2.java
@@ -86,14 +86,65 @@ public class HomeWork2 {
         // Например, дано: [1,1,1,1,1]
         // Ожидаемый результат: [0,1,0,1,0]
         // Подсказка: прочитай про операнд "%".
+        int[] array = { 1,1,1,1,1,1,1,1,1,1};
+        for (int i = 0; i < array.length; i++) {
+            if (i%2==0){
+                array[i]=0;
+            }
+            System.out.println(array[i]);
+        }
+        // Задание №3:
+        // Дано:
+        boolean hasFuel = false;
+        boolean hasElectricsProblem =true;
+        boolean hasMotorProblem = false;
+        boolean hasTransmissionProblem = false;
+        boolean hasWheelsProblem = true;
+        int countGarage = (hasFuel ? 1 : 0) + (hasElectricsProblem ? 1 : 0) + (hasMotorProblem ? 1 : 0) + (hasWheelsProblem ? 1 : 0);
+        System.out.println(countGarage);
+        int chek = 0;
+        // В автосервис приехала сломанная машина. Механики находят неисправность следующим способом:
+        // Если у машины нет бензина и ничего не сломано, то отдают машину владельцу и берут 1000 рублей за консультацию.
+        // Если у машины проблема с двигателем, то чинят и берут 10 000.
+        // Если у машины проблема с электрикой, то чинят и берут 5000.
+        // Если у машины проблема с коробкой передач, то чинят и берут 4000.
+        // Если у машины проблема с колесами, то чинят и берут 2000.
+        // Если две детали сломаны, то на общий счет идет скидка 10%.
+        // Если сломана коробка передач, и электрика или двигатель, то на общий счет скидка 20%.
+        // Если нет бензина и что-то сломано, то за консультацию денег не берут.
+        // Ситуации, что бензин есть и ничего не сломано - быть не может.
+        // Ожидаемый результат: выведен на экран счет клиенту.
 
+        if (!hasElectricsProblem & !hasMotorProblem & !hasTransmissionProblem & !hasWheelsProblem & hasFuel){
+               chek += 1000;
+            System.out.println("С Вас, за консультацию и бензин 1000 рублей ");
+        }
+        if (hasMotorProblem) {
+            chek += 10000;
+            System.out.println("У Вас сломан двигатель, за ремонт 10 000 рублей");
+        }
+        chek += 5000;
+        System.out.println("у Вас проблемы с электрикой, с вас 5000 рублей ");
+        if (hasTransmissionProblem) {
+            chek += 4000;
+            System.out.println("У вас проблемы с коробкой передач, с Вас 4000");
+        }
+        if (hasWheelsProblem) {
+            chek += 2000;
+            System.out.println("Если у машины проблема с колесами, то чинят и берут 2000" );
 
+        }
+        if ( countGarage == 2) {
+            double endPrice = chek/100*10;
+            System.out.println("Ваша скидка " + endPrice);
+            System.out.println("Итого:" + (chek-endPrice));
 
-
-
-
-
-
+        }
+        if (hasTransmissionProblem &&( hasElectricsProblem || hasMotorProblem )) {
+            double endPrice2 = chek/100*20;
+            System.out.println("Ваша скидка " + endPrice2);
+            System.out.println("Итого:" + (chek-endPrice2));
+        }
 
 
 
